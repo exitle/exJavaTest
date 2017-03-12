d3ac389a659b2b8adff3f743f77538a7acd46f3d
exitle <exitlee@outlook.com>
2017-03-12 10:16:04 +0100

    tools

--- Tools/src/Testing.java
+++ Tools/src/Testing.java
@@ -3,7 +3,9 @@ public class Testing {
 
 	public static void main(String[] args) {
 		FileControl fc = new FileControl("testing.txt");
-		fc.writeFile("test1");
+//		fc.writeFile("test1");
+		fc.readFile();
+		System.out.println(fc.printFile());
 	}
 
 }
